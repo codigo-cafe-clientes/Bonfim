bc6329d241cc7973f07c048c571cd2d45847a096
Gustavo Mathias <contato@codigo.cafe>
2022-12-25 03:54:40 -0300

    Feature: create Consulta component and add component in the Home Page

--- front/src/controller/Home/index.tsx
+++ front/src/controller/Home/index.tsx
@@ -2,6 +2,7 @@ import HomeSobre from "view/Home/Sobre";
 import HomeCliente from "view/Home/Cliente";
 import HomeVideo from "view/Home/Video";
 import CtaContact from "components/CtaContact";
+import HomeConsulta from "view/Home/Consulta";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HomeSobre />
       <HomeCliente />
       <HomeVideo />
+      <HomeConsulta />
       <CtaContact />
     </>
   );
---
+++ front/src/view/Home/Consulta/index.tsx
@@ -0,0 +1,12 @@
+import { HomeConsultaSection } from "./style";
+
+export default function HomeConsulta() {
+  return (
+    <HomeConsultaSection>
+      <div>Consulta</div>
+      <div>Facilitador contabil</div>
+      <div>Pessoa Juridica</div>
+      <div>Pessoa Física</div>
+    </HomeConsultaSection>
+  );
+}
