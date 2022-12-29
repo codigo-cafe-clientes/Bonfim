6401b4b55fd359582c9f42006aa5d5a5bf6d001f
Gustavo Mathias <contato@codigo.cafe>
2022-12-29 11:07:07 -0300

    Feature: add GlideJS in the Service Page

--- front/src/controller/Sobre/index.tsx
+++ front/src/controller/Sobre/index.tsx
@@ -3,8 +3,14 @@ import SobreConteudoPrincipal from "view/Sobre/ConteudoPrincipal";
 import SobreMvv from "view/Sobre/Mvv";
 import CtaContact from "components/CtaContact";
 import SobreConteudo from "view/Sobre/Conteudo";
+import GlideJS from "components/Glide";
 
 import HeroImage from 'assets/images/banner-default.png';
+import Sobre01 from 'assets/images/sobre01.jpg';
+import Sobre02 from 'assets/images/sobre02.jpg';
+import Sobre03 from 'assets/images/sobre03.jpg';
+import Sobre04 from 'assets/images/sobre04.jpg';
+import Sobre05 from 'assets/images/sobre05.jpg';
 
 interface Props {
   setTitle : React.Dispatch<React.SetStateAction<string | undefined>>,
@@ -15,12 +21,22 @@ export default function Sobre( { setTitle, setBackground }:Props ) {
 
   setTitle( 'Conheça nossa história' );
   setBackground( HeroImage );
+  const imagens = [ Sobre01, Sobre02, Sobre03, Sobre04, Sobre05 ];
 
   return (
     <>
       <SobreLogo />
       <SobreConteudoPrincipal />
       <SobreMvv />
+      <GlideJS 
+        images={imagens}
+        id={'sobre'}
+        bullets={true}
+        Type={'carousel'}
+        PerView={4}
+        StartAt={1}
+        Bound={true}
+      />
       <SobreConteudo />
       <SobreConteudo />
       <SobreConteudo />
