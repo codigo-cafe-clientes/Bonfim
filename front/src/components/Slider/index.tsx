fe34e352cae57925fb16f8d720bb3a43cd9a7a75
Gustavo Mathias <contato@codigo.cafe>
2022-12-30 09:26:02 -0300

    Partial: create the structure of the Slider

---
+++ front/src/components/Slider/index.tsx
@@ -0,0 +1,19 @@
+import datas from './data.json';
+
+export default function Slider() {
+
+  return (
+    <section>
+      <ul>
+        { datas.map(( item ) => (
+          <li key={item.id} data-image={item.image} data-content={item.content} >{item.title}</li>
+        )) }
+      </ul>
+      <div>
+        <h3>{datas[0].title}</h3>
+        <p>{datas[0].content}</p>
+        <a href="#">Saiba mais</a>
+      </div>
+    </section>
+  );
+}
--- front/src/controller/Home/index.tsx
+++ front/src/controller/Home/index.tsx
@@ -1,12 +1,14 @@
+import Slider from "components/Slider";
 import HomeSobre from "view/Home/Sobre";
 import HomeCliente from "view/Home/Cliente";
 import HomeVideo from "view/Home/Video";
-import CtaContact from "components/CtaContact";
 import HomeConsulta from "view/Home/Consulta";
+import CtaContact from "components/CtaContact";
 
 export default function Home() {
   return (
     <>
+      <Slider />
       <HomeSobre />
       <HomeCliente />
       <HomeVideo />
