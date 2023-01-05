b925a7ea7bd6a3f5132b2f128fe981296db4d811
Thiago Carvalho <thiagoabcarvalho@gmail.com>
2023-01-05 15:35:22 -0300

    estilização das páginas

--- front/src/view/Contato/Formulario/style.js
+++ front/src/view/Contato/Formulario/style.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
 
 export const ContatoFormularioSection = styled.section`
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    margin-top: 100px;
+    
+
+    label {
+      color: #3D3C3C;
+      font-weight:600;
+      font-size: 1.125rem;
+      margin-bottom: 34px;
+    }
+    
+    placeholder {}
+    
+    input {
+      padding: 12px 18px;
+      font-weight:600;
+      width: 600px;
+      height: 50px
+      
+      border-radius: 05px;
+      margin-bottom: 34px;
+      
+    }
+
+    
 `;
--- front/src/view/Sobre/Mvv/index.tsx
+++ front/src/view/Sobre/Mvv/index.tsx
@@ -1,6 +1,11 @@
+import ImgDesk from 'assets/images/photo-desk.png';
+import { SobreMvvSection } from './style';
+import { theme } from 'components/Layout/theme';
+
+
 export default function SobreMvv() {
   return (
-    <section>
+    <SobreMvvSection theme={theme}>
       <div>
         <h2>Missão</h2>
         <p>Entender as necessidades e oferecer aos nossos clientes, serviços fiscais,contábeis trabalhistas de qualidade, com rapidez e eficácia, para auxiliar nas decisões gerenciais, promover soluções seguras aos problemas e desafios da atividade empresarial moderna e demonstrar transparência profissional.</p>
@@ -13,6 +18,6 @@ export default function SobreMvv() {
         <h2>Valores</h2>
         <p>Respeito às pessoas, aos colaboradores e à sociedade em qualquer situação. Responsabilidade com os clientes e profissionais. Ética e Transparência nos negócios e no ambiente de trabalho. Qualidade desde o atendimento até a solução das demandas dos clientes. Foco em Resultados.</p>
       </div>
-    </section>
+    </SobreMvvSection>
   );
 }
