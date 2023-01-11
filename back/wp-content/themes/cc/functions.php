e79a4c3aad5b715459a19c4fc62f49b478e3f19b
Gustavo Mathias <contato@codigo.cafe>
2023-01-11 08:12:19 -0300

    Feature: create new route for clients rest

--- back/wp-content/themes/cc/functions.php
+++ back/wp-content/themes/cc/functions.php
@@ -40,6 +40,7 @@
 			'labels' => $labels,
 			'public' => true,
 			'hierarchical' => false,
+			'rest_namespace' => 'cb/v1',
 			'rest_base' => 'clients',
 			'show_in_rest' => true,
 			'menu_icon' => 'dashicons-groups',
@@ -53,4 +54,33 @@
 	add_action( 'after_setup_theme', 'add_thumbnail_support' );
 	function add_thumbnail_support() {
 		add_theme_support( 'post-thumbnails' );
+	}
+
+	add_action( 'rest_api_init', 'create_endpoint_clients' );
+	function create_endpoint_clients () {
+		register_rest_route('bc/v1', '/clients', [
+			'method' => 'GET',
+			'callback' => function( $data ) {
+				$datas = [];
+				$args = Array(
+					'post_type' => 'bc_client',
+					'posts_per_page' => -1
+				);
+				$clients = new WP_Query( $args );
+				if ( $clients->have_posts() ) {
+					while ( $clients->have_posts() ) {
+						$clients->the_post();
+						$client['id'] = get_the_ID();
+						$client['name'] = get_the_title();
+						$client['image'] = change_media_uri( get_the_post_thumbnail_url( get_the_ID(), 'full' ) );
+						$datas[] = $client;
+					}
+				}
+				return $datas;
+			}
+		]);
+	}
+
+	function change_media_uri ( $url ) {
+		return str_replace( 'https://panel.contabilidadebonfim.com.br/cdn', 'https://cdn.contabilidadebonfim.com.br', $url );
 	}
